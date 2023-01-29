b279c348a501bb281e3bb21482a209a015cad090
Märten S <marten.salong@tktk.ee>
2023-01-29 17:06:00 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//Rakendus küsib kasutajalt tema nime
+//Rakendus tervitab kaustajat nimepidi
+
+Console.WriteLine("Enter Your name:"); //Output
+//String - sõne
+String userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!");  //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
